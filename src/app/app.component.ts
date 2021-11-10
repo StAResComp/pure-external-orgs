7e7f7c1c278a1fa05df40c8f89a0870a523f1028
Patrick McCann <pgm5@st-andrews.ac.uk>
2021-11-10 16:57:40 +0000

    Improves UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, Injectable } from '@angular/core';
-import { Pure, ApiKey } from './pure.model';
+import { Pure, ApiKey, ExternalOrganization } from './pure.model';
 import { PureService } from './pure.service';
 
 @Component({
@@ -13,25 +13,25 @@ export class AppComponent {
   public pureUrl = 'https://riswebtest.st-andrews.ac.uk';
   public apiKey = '';
   public searchString = '';
-  public results?: any = null;
+  public results?: Array<ExternalOrganization> = [];
   public busy = false;
 
   constructor(private pureService: PureService){}
 
   public search(): void {
-    this.busy = true;
-    this.pureService.pure = new Pure(new ApiKey(this.apiKey), this.pureUrl);
-    this.pureService.extOrgSearch(this.searchString).subscribe(
-      (response) => {
-        this.results = response.items;
-        console.log(this.results);
-        this.busy = false;
-      },
-      (error) => {
-        console.log(error);
-        this.busy = false;
-      }
-    );
+    if (!this.busy) {
+      this.busy = true;
+      this.pureService.pure = new Pure(new ApiKey(this.apiKey), this.pureUrl);
+      this.pureService.extOrgSearch(this.searchString).subscribe(
+        (response) => {
+          this.results = response.items;
+          this.busy = false;
+        },
+        (error) => {
+          console.log(error);
+          this.busy = false;
+        }
+      );
+    }
   }
-
 }
